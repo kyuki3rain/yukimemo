bd1f98091cc070b8e2d8f5116d483a1f519dda24
kyuki3rain <kyuki3rain>
2022-04-03 15:26:51 +0900

    ver0.9.0 androidリリース

--- lib/models/memo.dart
+++ lib/models/memo.dart
@@ -1,8 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 // Import the firebase_core and cloud_firestore plugin
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:yukimemo/auth_controller.dart';
 
 class Memo {
   late String uuid;
@@ -20,14 +22,24 @@ class Memo {
 
 class MemoModel extends ChangeNotifier {
   late List<Memo> _listItems = [];
-  final memos = FirebaseFirestore.instance.collection('memos');
+  final Reader _read;
+  final users = FirebaseFirestore.instance.collection('users');
+  DateTime listUpdated = DateTime.now();
+
+  MemoModel(this._read);
 
   int get length => _listItems.length;
 
   Future<void> fetchItems() async {
+    // if (listUpdated.difference(DateTime.now()).inSeconds < 5) return;
+
+    print("firestore use fetch.");
+    User? user = _read(authControllerProvider);
+    final memos = users.doc(user?.uid).collection('memos');
     final docs = await memos.get();
     final _listItems = docs.docs.map((doc) => Memo.doc(doc)).toList();
     this._listItems = _listItems;
+    listUpdated = DateTime.now();
     notifyListeners();
   }
 
@@ -40,6 +52,9 @@ class MemoModel extends ChangeNotifier {
   }
 
   Future<String> add(String title, String content) async {
+    print("firestore use add.");
+    User? user = _read(authControllerProvider);
+    final memos = users.doc(user!.uid).collection('memos');
     DocumentReference value =
         await memos.add({'title': title, 'content': content});
     DocumentSnapshot doc = await value.get();
@@ -50,6 +65,9 @@ class MemoModel extends ChangeNotifier {
   }
 
   void remove(Memo memo) {
+    print("firestore use remove.");
+    User? user = _read(authControllerProvider);
+    final memos = users.doc(user!.uid).collection('memos');
     memos.doc(memo.uuid).delete();
     int index = _listItems.indexWhere((element) => element.uuid == memo.uuid);
     _listItems.removeAt(index);
@@ -57,6 +75,9 @@ class MemoModel extends ChangeNotifier {
   }
 
   void update(Memo memo) {
+    print("firestore use update.");
+    User? user = _read(authControllerProvider);
+    final memos = users.doc(user!.uid).collection('memos');
     memos.doc(memo.uuid).set({'title': memo.title, 'content': memo.content});
     int index = _listItems.indexWhere((element) => element.uuid == memo.uuid);
     _listItems[index] = memo;
@@ -64,4 +85,4 @@ class MemoModel extends ChangeNotifier {
   }
 }
 
-final memoProvider = ChangeNotifierProvider((ref) => MemoModel());
+final memoProvider = ChangeNotifierProvider((ref) => MemoModel(ref.read));
--- lib/screens/memo_list.dart
+++ lib/screens/memo_list.dart
@@ -1,11 +1,12 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import '../components/create_memo_dialog.dart';
 import '../models/memo.dart';
 
-class MemoListPage extends ConsumerWidget {
+class MemoListPage extends HookConsumerWidget {
   const MemoListPage({
     Key? key,
     required this.user,
@@ -15,32 +16,39 @@ class MemoListPage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    useEffect(() {
+      ref.read(memoProvider).fetchItems();
+      return null;
+    }, []);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('yukimemo'),
       ),
-      body: FutureBuilder(
-          future: ref.read(memoProvider).fetchItems(),
-          builder: (context, snapshot) {
-            return ListView.builder(
-                itemCount: ref.watch(memoProvider).length,
-                itemBuilder: (context, index) {
-                  Memo memo = ref.watch(memoProvider).getMemoWithIndex(index);
-                  return Card(
-                      child: ListTile(
-                    onTap: () {
-                      Navigator.of(context)
-                          .pushNamed('/edit', arguments: memo.uuid);
-                    },
-                    title: Text(memo.title),
-                    trailing: IconButton(
-                        icon: const Icon(Icons.delete_forever),
-                        onPressed: () {
-                          ref.read(memoProvider).remove(memo);
-                        }),
-                  ));
-                });
-          }),
+      body: RefreshIndicator(
+        onRefresh: () async {
+          await ref.read(memoProvider).fetchItems();
+        },
+        child: ListView.builder(
+            physics: const AlwaysScrollableScrollPhysics(),
+            itemCount: ref.watch(memoProvider).length,
+            itemBuilder: (context, index) {
+              Memo memo = ref.watch(memoProvider).getMemoWithIndex(index);
+              return Card(
+                  child: ListTile(
+                onTap: () {
+                  Navigator.of(context)
+                      .pushNamed('/edit', arguments: memo.uuid);
+                },
+                title: Text(memo.title),
+                trailing: IconButton(
+                    icon: const Icon(Icons.delete_forever),
+                    onPressed: () {
+                      ref.read(memoProvider).remove(memo);
+                    }),
+              ));
+            }),
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () => showDialog(
           context: context,
